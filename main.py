416bd687cceab316e7425f123ae95cbd4861374a
Nuthan B <156648630+Techno-Furious@users.noreply.github.com>
2024-10-03 20:00:40 +0530

    Database

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import string
 import streamlit as st
 import time
 import random
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase: Client = create_client(url, key)
 
 def calculate_score(password):
     score = 0
@@ -33,12 +43,18 @@ st.markdown('<h1 style="color:#B22222;">Check Your Password Strength</h1>', unsa
 st.write("This app will check if your password has been breached before and will calculate your password strength.")
 password = st.text_input("Enter a password", type="password")
 
-with open ("keysCleaned.txt",'r',encoding='utf-8') as file:
-    key_words = file.read().splitlines()
-for i in range(2):
-    selected_words = random.choices(key_words, k=2)
-    selected_words = [word.capitalize() for word in selected_words]
+# with open ("keysCleaned.txt",'r',encoding='utf-8') as file:
+#     key_words = file.read().splitlines()
+# for i in range(2):
+#     selected_words = random.choices(key_words, k=2)
+#     selected_words = [word.capitalize() for word in selected_words]
 
+rad1=random.randint(2, 8900)
+response = supabase.table("key_words").select("key").eq("id", rad1).execute()
+rad2=random.randint(2, 8900)
+response2 = supabase.table("key_words").select("key").eq("id", rad2).execute()
+selected_words = [response.data[0]['key'] if response.data else None, response2.data[0]['key'] if response2.data else None]
+selected_words = [word.capitalize() for word in selected_words]
 
 def generate_password(keys_list=selected_words):
 
@@ -72,7 +88,6 @@ if st.button("Check"):
     else:
         process=st.empty()
         process.write("Checking password...")
-        time.sleep(1)
         with open ("common.txt", "r",encoding="utf-8") as file:
             common_passwords = file.read().splitlines()
         if password in common_passwords:
@@ -108,7 +123,6 @@ if st.button("Generate"):
         generated=st.empty()
         generation=st.empty()
         generation.write("Generating password...")
-        time.sleep(1)
         password = generate_password()
         generation.empty()
         generated.write("Generated password:")
@@ -117,7 +131,6 @@ if st.button("Generate"):
         generated=st.empty()
         generation=st.empty()
         generation.write("Generating password...")
-        time.sleep(1)
         password = generate_password(user_keys)
         generation.empty()
         generated.write("Generated password:")
